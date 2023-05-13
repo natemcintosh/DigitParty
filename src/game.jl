a0d3f6cb90facb4ff352f6196225d33773846ccf
Nathan McIntosh <natemcintosh@gmail.com>
2023-05-13 13:41:39 -0400

    closer to done with game

--- src/game.jl
+++ src/game.jl
@@ -2,7 +2,7 @@
 using StaticArrays
 import Base: print, show
 
-export Game, make_move!
+export Game, make_move!, get_empty_spots, game_is_over
 
 mutable struct Game
     board   :: MMatrix{5,5,Int8}
@@ -22,20 +22,32 @@ function Base.print(io::IO, g::Game)
         end
         println(io)
     end
-    print(io, "\nUp next: $(g.up_next[1]), $(g.up_next[2])")
+    print(io, "\nUp next: $(g.up_next[1]), then $(g.up_next[2])")
 end
 
 Base.print(g::Game) = Base.print(Base.stdout, g)
 
-function make_move!(g::Game, row::Int, col::Int; verbose::Bool = false)
+"""
+    make_move!(g::Game, row::Int, col::Int; verbose::Bool = false)
+
+Make a move by placing the first item from `up_next` at `[row, col]`
+Optionally, print the status of the board and up_next if so desired
+"""
+make_move!(g::Game, row::Int, col::Int; verbose::Bool = false) =
+    make_move!(g, CartesianIndex(row, col), verbose = verbose)
+
+"""
+    make_move!(g::Game, idx; verbose::Bool = false)
+
+`idx` can be either LinearIndex or CartesianIndex
+"""
+function make_move!(g::Game, idx::Union{CartesianIndex{2}, IndexLinear, Int}; verbose::Bool = false)
     # Check validity of input
-    # `row` and `col` each have to be in [1, 5]
-    ((row in 1:5) && (col in 1:5)) || BoundsError(g.board, CartesianIndex(row, col))
     # They cannot place a piece where one already exists
-    @assert g.board[row, col] < 1 "Number already at [$row, $col]"
+    @assert g.board[idx] < 1 "Number already at [$idx]"
 
     # Place the first number from `up_next` at the desired location
-    g.board[row, col] = g.up_next[1]
+    g.board[idx] = g.up_next[1]
 
     # Update `up_next`
     g.up_next[1] = g.up_next[2]
@@ -43,4 +55,22 @@ function make_move!(g::Game, row::Int, col::Int; verbose::Bool = false)
 
     # Print if asked
     verbose && print(g)
+end
+
+"""
+    get_empty_spots(g::Game)
+
+For API use
+"""
+function get_empty_spots(g::Game)
+    findall(==(0), g.board)
+end
+
+"""
+    game_is_over(g::Game)
+
+Check if the game is over
+"""
+function game_is_over(g::Game)::Bool
+    all(>=(1), g.board)
 end
--- test/game.jl
+++ test/game.jl
@@ -15,3 +15,38 @@ end
     make_move!(g, 1, 1)
     @test_throws AssertionError make_move!(g, 1, 1)
 end
+
+@testset "get_empty_spots" begin
+    g = Game()
+
+    # Everything is empty
+    want = (1:5, 1:5) |> CartesianIndices |> vec
+    got  = get_empty_spots(g)
+    @test want == got
+
+    # Add a number to [1, 1]
+    want = setdiff(want, [CartesianIndex(1, 1)])
+    make_move!(g, 1, 1)
+    got = get_empty_spots(g)
+    @test want == got
+
+    # Add a number to [3, 4]
+    want = setdiff(want, [CartesianIndex(3, 4)])
+    make_move!(g, 3, 4)
+    got = get_empty_spots(g)
+    @test want == got
+
+    @test_broken @inferred get_empty_spots(g)
+end
+
+@testset "game_is_over" begin
+    g = Game()
+    @test !game_is_over(g)
+
+    # Fill up the board
+    for idx in eachindex(g.board)
+        make_move!(g, idx)
+    end
+
+    @test game_is_over(g)
+end
